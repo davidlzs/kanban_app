7bab03806adaf7ed649ebd39c20e4c14604157e9
bitnami <bitnami@ubuntu.defaultdomain>
2016-02-06 14:51:07 -0600

    add css-loader, style-loader; setup webpack-merge

--- app/component.js
+++ app/component.js
@@ -1,5 +1,5 @@
 module.exports = function() {
   var element = document.createElement('h1');
-  element.innerHTML = '<div>Hello David</div><div>Hello Felix.</div>';
+  element.innerHTML = '<div>Hello David, yay</div><div>Hello Felix.</div>';
   return element;
 };
