0e3b3248aa10a0a8ff82bb91f13039412168a842
bitnami <davidlzs@hotmail.com>
2016-02-09 20:33:17 -0600

    extract notes to state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,23 +1,29 @@
 import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Notes.jsx';
+import Notes from './Notes.jsx';
 
-const notes = [
-  {
-    id: uuid.v4(),
-    task: 'Learn Javascript'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn NPM'
-  },
-  {
-    id: uuid.v4(),
-    task: 'Learn React'
-  }
-];
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state ={
+      notes : [
+        {
+          id: uuid.v4(),
+          task: 'Play Javascript'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Play NPM'
+        },
+        {
+          id: uuid.v4(),
+          task: 'Play React'
+        }
+      ]
+    };
+  }
   render() {
+    const notes = this.state.notes;
     return (
       <div>
         <Notes items={notes} />
