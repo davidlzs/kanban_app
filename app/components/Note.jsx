6a6ac181cbaf98e092b9c00ccd927df0a2e7d6c6
bitnami <davidlzs@hotmail.com>
2016-02-09 21:30:04 -0600

    add edit note/task skeleton

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
 
 export default class Note extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      editing: false
+    };
+    this.edit = this.edit.bind(this);
+    this.finishEdit = this.finishEdit.bind(this);
+    this.checkEnter = this.checkEnter.bind(this);
+    this.renderEdit = this.renderEdit.bind(this);
+    this.renderNote = this.renderNote.bind(this);
+  }
   render() {
-    return   <div>{this.props.task}</div>
+    const editing = this.state.editing;
+    return  <div>{editing ? this.renderEdit() : this.renderNote()}</div>
+  }
+  edit() {
+    this.setState({
+      editing:true
+    });
+  }
+  renderEdit() {
+    return <input type="text"
+            autoFocus={true}
+            defaultValue={this.props.task}
+            onBlur={this.finishEdit}
+            onKeyPress={this.checkEnter}/>;
+  }
+  renderNote() {
+    return  <div onClick={this.edit}>{this.props.task}</div>;
+  }
+  finishEdit(e) {
+    //this.onEdit(e.target.value);
+    this.setState({editing : false});
+  }
+  checkEnter(e) {
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
   }
 }
